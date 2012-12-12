673a6c0195fc7f50790552e03aa8b7c1bcc26ae8
boyska <piuttosto@logorroici.org>
2012-12-12 14:54:47 +0100

    compiler tarball extension handling

--- compiler.sh
+++ compiler.sh
@@ -3,7 +3,7 @@
 function out_selfextract() {
 	echo '#!/usr/bin/env bash'
 	cat pre.sh
-	add_tar src.tar.xz /tmp/asd xz
+	add_tar src.tar.xz /tmp/asd
 	cat post.sh
 }
 
@@ -11,8 +11,19 @@ function add_tar() {
 #$1 is filename
 #$2 is output dir
 #$3 is compression option
+	if [ -z "$3" ]; then
+		case $1 in
+			*.xz)	format=xz ;;
+			*.gz)	format=gzip ;;
+			*.bzip3)	format=bzip2 ;;
+			*.tar)	format="" ;;
+			*) echo "Can't handle file" >&2 ; exit 1 ;;
+		esac
+	else
+		format=$3
+	fi
 	echo "# START: tarball $1"
-	echo "base64 -d <<EOTAR |tar --$3 -C$2 -xf -"
+	echo "base64 -d <<EOTAR |tar ${format:+--$format} -C$2 -xf -"
 	base64 $1
 	echo 'EOTAR'
 	echo "# END: tarball $1"
